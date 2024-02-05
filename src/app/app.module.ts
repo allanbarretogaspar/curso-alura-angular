90b5c16cd680f43731a62ac37cea67aa6c92a550
Allan Barreto Gaspar <allan.gaspar@amcom.com.br>
2024-02-05 09:11:03 -0300

    finalizando curso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/yes-no-button-group.module';
+import { DisableControlModule } from './shared/directives/disable-control/disable-control.module';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { YesNoButtonGroupModule } from './shared/components/yes-no-button-group/
     BrowserModule,
     YesNoButtonGroupModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    DisableControlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
